42f7c1f4e30925445d9d6cd66a160a6fd92c4092
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2022-09-10 22:00:51 +0530

    sumOfNaturalNumbers

--- Basic Assignments/sumOfNaturalNumbers.java
+++ Basic Assignments/sumOfNaturalNumbers.java
@@ -5,6 +5,6 @@ public class sumOfNaturalNumbers {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.print(n*(n+1))/2);
+        System.out.print(n*(n+1)/2);
     }
 }
