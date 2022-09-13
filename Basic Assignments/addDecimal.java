9ae17796388d7da181b312f89dffc4c48a03cf4b
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2022-09-13 22:29:46 +0530

    changed

---
+++ Basic Assignments/addDecimal.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class addDecimal {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        float f=sc.nextFloat();
+        int a=sc.nextInt();
+        System.out.println(a+f);
+    }
+}
--- Basic Assignments/pascalTriangle.java
+++ Basic Assignments/pascalTriangle.java
@@ -1,27 +1,17 @@
 import java.util.*;
 public class pascalTriangle {
-    public static int fact(int n){
-        // int facto=1;
-        // for(int i=2;i<=n;i++){
-        //     facto*=i;
-        // }
-        // return facto;
-        if(n==0)return 1;
-        return n*fact(n-1);
-    }
-    static int res(int n,int r){
-        return fact(n)/fact(n-r)*fact(r);
-    }
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        //int f=fact(n);
+        int[][] arr=new int[n][n];
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=n-i;j++){
-                System.out.print(" ");
-            }
             for(int j=0;j<=i;j++){
-                System.out.print(" "+res(i,j));
+                if(j==0 || i==j){
+                    arr[i][j]=1;
+                }else{
+                    arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+                }
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
