6877eef2ebfd8bd3b071bacf66d04c0c9346c79a
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2022-09-15 10:46:53 +0530

    assignment

---
+++ Sorting algorithms/selectionSort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class selectionSort {
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int minIndex=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[minIndex]>arr[j]){
+                    minIndex=j;
+                }
+            }
+            int temp=arr[minIndex];
+            arr[minIndex]=arr[i];
+            arr[i]=temp;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
