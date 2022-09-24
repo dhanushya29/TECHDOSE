6d65148e0efeac28802b3467c51dc11d21a4f3ec
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2022-09-24 21:56:49 +0530

    assignments

--- Arrays/binarySearchBinarySearch.java
+++ Arrays/binarySearchBinarySearch.java
@@ -4,7 +4,7 @@ public class binarySearchBinarySearch {
     public static void main(String[]args) {
         Scanner sc=new Scanner(System.in);
         int []arr={1,2,3,4,5};
-        int low=0,high=4,search=4;
+        int low=0,high=4,search=1;
         while(low<high){
             int mid=low+(high-low)/2;
             if(arr[mid]<search){
