6a2470cb9b62a13e0d534b44bca79b843a309f1b
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2022-09-24 21:51:10 +0530

    assignments

---
+++ Arrays/binarySearchBinarySearch.java
@@ -0,0 +1,22 @@
+package TECHDOSE.Arrays;
+import java.util.*;
+public class binarySearchBinarySearch {
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        int []arr={1,2,3,4,5};
+        int low=0,high=4,search=4;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<search){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
+        }
+        if(arr[low]==search || arr[high]==search){
+            System.out.println("Found");
+        }else{
+            System.out.println("Not Found");
+        }
+    }
+}
