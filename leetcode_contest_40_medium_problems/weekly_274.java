2aa15bd5d1a5f5404fe7d7505d8a16ca4aa0188c
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2022-10-20 21:24:34 +0530

    assignments

---
+++ leetcode_contest_40_medium_problems/weekly_274.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class weekly_274 {
+    // class Solution {
+        public long smallestNumber(long num) {
+            if(num==0)return 0;
+            boolean isNegative=num<0;
+            num =num<0?num*-1 : num;
+            String str;
+            char[]ch=String.valueOf(num).toCharArray();
+            Arrays.sort(ch);
+                    // System.out.print(Arrays.toString(ch));
+            if(!isNegative){
+                int non=0;
+                for(;non<ch.length;non++){
+                    if(ch[non]!='0'){
+                        break;
+                    }
+                }
+                // System.out.println(ch[0]+" "+non);
+                char temp=ch[0];
+                ch[0]=ch[non];
+                ch[non]=temp;
+                str=new String(ch);
+            }
+            else{
+                str=new String(ch);
+                StringBuilder sb=new StringBuilder(str);
+                str="-".concat(sb.reverse().toString());
+            }
+            return Long.valueOf(str);
+        }
+    // }
+}
