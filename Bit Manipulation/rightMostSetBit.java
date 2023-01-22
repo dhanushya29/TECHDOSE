1917b49d5d3f10400053ff34b6438db2959876ed
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2023-01-22 22:09:45 +0530

    rightMostSetBit

--- Bit Manipulation/rightMostSetBit.java
+++ Bit Manipulation/rightMostSetBit.java
@@ -9,13 +9,6 @@ public class rightMostSetBit {
         // BufferedReader Class for Fast buffer Input
         File inFile = new File("C:\\Coding\\input.txt");
         Scanner sc = new Scanner(inFile);
-
-        // PrintWriter class prints formatted representations
-        // of objects to a text-output stream.
-        // PrintWriter pw=new PrintWriter(new
-        //         BufferedWriter(new FileWriter("C:\\Coding\\output.txt")));
-        // same print or println can be used
-        // Your code goes Here
         int n=sc.nextInt();
         //rightmostSetBit
         while((n&1)!=1){
