5a44b0c501e2d5baf23dfe0aaec4749eeced3c79
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2023-01-24 00:30:16 +0530

    assignments

---
+++ Bit Manipulation/flipBit.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class flipBit {
+    public static void main(String[] args) throws IOException {
+
+
+        // BufferedReader Class for Fast buffer Input
+        File inFile = new File("C:\\Coding\\input.txt");
+        Scanner sc = new Scanner(inFile);
+
+        // PrintWriter class prints formatted representations
+        // of objects to a text-output stream.
+        // PrintWriter pw=new PrintWriter(new
+        //         BufferedWriter(new FileWriter("C:\\Coding\\output.txt")));
+        // same print or println can be used
+        // Your code goes Here
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        // end of the code
+        // pw.flush();
+        System.out.println(n^(1<<k));
+    }
+}
