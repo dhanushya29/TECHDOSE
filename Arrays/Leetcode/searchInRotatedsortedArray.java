d245986c1f7fd24681044c0ad8c21fcb801bd978
Dhanushya <95904394+dhanushya29@users.noreply.github.com>
2023-01-30 00:13:53 +0530

    search

---
+++ Arrays/Leetcode/searchInRotatedsortedArray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int low=0,high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target)return mid;
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=target && arr[mid]>=target){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(arr[mid]<=target && arr[high]>=target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
